3cdde973377c6803856d27f275aa091b6cf27fc8
José Padilla <jpadilla@webapplicate.com>
2013-03-22 09:46:35 -0400

    Update subscribe tester

--- kippt_reader/subscribe.py
+++ kippt_reader/subscribe.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from libs.djpubsubhubbub.models import Subscription
 
 sub = Subscription.objects.subscribe(
-    topic='http://push-pub.appspot.com/feed/',
+    topic='http://push-pub.appspot.com/feed',
     hub=settings.SUPERFEEDR_HUB,
-    debug=True
+    debug=True,
+    verify='async'
 )
