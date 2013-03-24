2b8674528972655937eff797e61fa6819bfc3ba8
José Padilla <jpadilla@webapplicate.com>
2013-03-24 09:44:11 -0400

    Handle new content from SuperFeedr to Kippt

--- apps/feeds/signals.py
+++ apps/feeds/signals.py
@@ -1,13 +1,31 @@
 from libs.djpubsubhubbub.signals import updated
 
+from .models import Feed
+
 
 def update_handler(sender, update, **kwargs):
     """
     Process new content being provided from SuperFeedr
 
     """
-    print sender
-    for entry in update.entries:
-        print entry
+
+    print sender.topic
+
+    users = []
+    feeds = Feed.objects.filter(feed_url=sender.topic)
+
+    for feed in feeds:
+        if feed.created_by not in users:
+            users.append(feed.created_by)
+
+    for user in users:
+        kippt = user.kippt_client()
+
+        for entry in update.entries:
+            title = entry['title']
+            summary = entry['summary']
+            link = entry['link']
+
+            kippt.addClip(link, user.list_id, title=title, notes=summary)
 
 updated.connect(update_handler, dispatch_uid='superfeedr')
