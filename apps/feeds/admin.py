47cc9796e16527a16df5a82aeab1d9e6ed6e4518
José Padilla <jpadilla@webapplicate.com>
2013-03-26 17:52:26 -0400

    Add FeedEntry custom model admin

--- apps/feeds/admin.py
+++ apps/feeds/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Feed, FeedEntry
 
+
+class FeedEntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link', 'feed', 'feed', 'added_to_kippt')
+
+
 admin.site.register(Feed)
-admin.site.register(FeedEntry)
+admin.site.register(FeedEntry, FeedEntryAdmin)
