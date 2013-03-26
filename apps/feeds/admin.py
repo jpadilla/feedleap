6f69a770ef3b55a7d846abfc306e41025131d8a6
José Padilla <jpadilla@webapplicate.com>
2013-03-26 18:25:49 -0400

    Fix FeedEntry custom model admin

--- apps/feeds/admin.py
+++ apps/feeds/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 from .models import Feed, FeedEntry
 
 
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('feed_url', 'created_by')
+
+
 class FeedEntryAdmin(admin.ModelAdmin):
     list_display = ('title', 'link', 'feed',
-                    'feed__created_by', 'added_to_kippt')
+                    'feed_created_by', 'added_to_kippt')
+
+    def feed_created_by(self, obj):
+        return obj.feed.created_by
+    feed_created_by.short_description = 'Created by'
 
 
-admin.site.register(Feed)
+admin.site.register(Feed, FeedAdmin)
 admin.site.register(FeedEntry, FeedEntryAdmin)
