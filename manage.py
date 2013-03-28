8c19549071782f3a07f1a6f1656817bb36b00dbe
José Padilla <jpadilla@webapplicate.com>
2013-03-28 10:17:13 -0400

    Read from local environment file if available

--- manage.py
+++ manage.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
+
+
+def read_env():
+    """Pulled from Honcho code with minor updates, reads local default
+    environment variables from a .env file located in the project root
+    directory.
+
+    """
+    try:
+        with open('.env') as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feed_to_kippt.settings")
 
     from django.core.management import execute_from_command_line
 
+    read_env()
     execute_from_command_line(sys.argv)
