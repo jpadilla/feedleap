dad1422d055cabf63f0ed96bf5858d2669ad20a6
José Padilla <jpadilla@webapplicate.com>
2013-03-30 17:43:19 -0400

    Add management command for subscriptions

---
+++ vendor/djpubsubhubbub/djpubsubhubbub/management/commands/subscriptions.py
@@ -0,0 +1,35 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from djpubsubhubbub.models import Subscription
+
+
+class Command(BaseCommand):
+    args = '<topic_url topic_url ...>'
+    help = 'Subscribes/Unsubscribes to a topic URL'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--unsubscribe',
+            action='store_true',
+            dest='unsubscribe',
+            default=False,
+            help='Unsubscribe a topic URL instead of susbscribing to it.'),
+    )
+
+    def handle(self, *args, **options):
+        for topic_url in args:
+            if options.get('unsubscribe', False):
+                sub = Subscription.objects.subscribe(
+                    topic=topic_url,
+                    hub=settings.SUPERFEEDR_HUB,
+                )
+
+                self.stdout.write('Successfully unsubscribed %s' % sub)
+            else:
+                sub = Subscription.objects.unsubscribe(
+                    topic=topic_url,
+                    hub=settings.SUPERFEEDR_HUB,
+                )
+
+                self.stdout.write('Successfully unsubscribed %s' % sub)
