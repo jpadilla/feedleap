5cbae4953ea0bde97b0cf7cd914a3a5263fa74be
José Padilla <jpadilla@webapplicate.com>
2013-03-30 17:43:41 -0400

    Make base module default DJANGO_SETTINGS_MODULE

--- manage.py
+++ manage.py
@@ -29,7 +29,7 @@ def read_env():
             os.environ.setdefault(key, val)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedleap.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedleap.settings.base")
 
     from django.core.management import execute_from_command_line
 
